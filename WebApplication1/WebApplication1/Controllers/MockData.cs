3255a15157a14b960211dae96eb4d611e0406f7a
Vilde Vevatne <v-viveva@microsoft.com>
2018-09-04 13:51:42 -0700

    Adds mock data objects

---
+++ WebApplication1/WebApplication1/Controllers/MockData.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Controllers
+{
+    public class MockData
+    {
+        public int Value { get; set; }
+        public string Condition { get; set; }
+        public string Object { get; set; }
+    }
+}
